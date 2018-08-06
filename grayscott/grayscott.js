27a1e1d3ad0b071cdddfc447d0d22899498389f8
simianstyle <simianstyle@users.noreply.github.com>
2018-08-05 20:14:04 -0700

    Update grayscott.js

--- grayscott/grayscott.js
+++ grayscott/grayscott.js
@@ -34,62 +34,9 @@ var mToggled = false;
 
 var mMinusOnes = new THREE.Vector2(-1, -1);
 
-// Some presets.
-var presets = [
-    { // Default
-        feed: 0.018,
-        kill: 0.054       
-    },
-/*    { // Solitons
-        feed: 0.03,
-        kill: 0.062
-    },
-    { // Pulsating solitons
-        feed: 0.025,
-        kill: 0.06
-    },
-    { // Worms.
-        feed: 0.078,
-        kill: 0.061
-    },
-    { // Mazes
-        feed: 0.029,
-        kill: 0.057
-    },
-    { // Holes
-        feed: 0.039,
-        kill: 0.058
-    },
-    { // Chaos
-        feed: 0.026,
-        kill: 0.051
-    },
-    { // Chaos and holes (by clem)
-        feed: 0.034,
-        kill: 0.056
-    },
-    { // Moving spots.
-        feed: 0.014,
-        kill: 0.054
-    },
-    { // Spots and loops.
-        feed: 0.018,
-        kill: 0.051
-    },
-    { // Waves
-        feed: 0.014,
-        kill: 0.045
-    },
-    { // The U-Skate World
-        feed: 0.062,
-        kill: 0.06093
-    }
-    */
-];
-
 // Configuration.
-var feed = presets[0].feed;
-var kill = presets[0].kill;
+var feed = 0.017;
+var kill = 0.054;
 
 init = function()
 {
